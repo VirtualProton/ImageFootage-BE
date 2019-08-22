56384374b16bd68d8b0b8cd5e44b0eb754ab840b
unknown <srinivas@conceptualpictures.com>
2019-08-22 17:54:47 +0530

    login ui and admin user migrations

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
-    public function dashboard()
-    {
+    public function dashboard(){    
         return view('dashboard');
     }
+	public function login(){
+		return view('adminlogin');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('admin/dashboard', 'Admin\DashboardController@dashboard');
+Route::get('admin', 'Admin\DashboardController@login');
