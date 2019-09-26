ab91bbace703870b0c9bd2ebd41284fc15d28638
amitpathak <amitpathak@mobikasa.com>
2019-09-27 02:35:19 +0530

    check issues

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,55 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { ProductComponent } from './product/product.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-
-const appRoutes: Routes = [
-  {
-    path: 'products',
-    component: ProductComponent,
-    data: { title: 'Product List' }
-  },
-  {
-    path: 'product-details/:id',
-    component: ProductDetailComponent,
-    data: { title: 'Product Details' }
-  },
-  {
-    path: 'product-add',
-    component: ProductAddComponent,
-    data: { title: 'Product Add' }
-  },
-  {
-    path: 'product-edit/:id',
-    component: ProductEditComponent,
-    data: { title: 'Product Edit' }
-  },
-  { path: '',
-    redirectTo: '/products',
-    pathMatch: 'full'
-  }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    ProductAddComponent,
-    ProductDetailComponent,
-    ProductEditComponent
+    AppComponent
   ],
   imports: [
-	RouterModule.forRoot(appRoutes),
-	FormsModule,
     BrowserModule,
-    AppRoutingModule,
-	HttpClientModule 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
