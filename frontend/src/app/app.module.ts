d1ee3f842624fb8e729b096488a6bbb68d6dd526
AKSrinivas <srinivas@conceptualpictures.com>
2019-09-27 18:20:53 +0530

    build

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ProductComponent } from './product/product.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    data: { title: 'Product List' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+  	FormsModule,
+ 	BrowserModule,
+  	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
