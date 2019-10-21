56fa259ed21b5d9f9f5bcaf38d15fd7cb5d41343
amit <amitp@pixelvide.com>
2019-10-21 09:39:10 +0530

    pond5 api

---
+++ backend/app/Http/Pond5/FootageApi.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Http\Pond5;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+class FootageApi {
+
+     private  $api_key =  'B349jWEnFUhd299';
+     private $api_secret= 'EXF3xRUA299';
+     private $url = "https://www.pond5.com/?page=api";
+     
+     public function  __construct(){
+
+     }
+   
+
+    private function str_random($len = 8, $allowed_charset=null) {
+        if($allowed_charset === null){
+           $allowed_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        }
+        return substr(str_shuffle($allowed_charset), 0, $len);
+    }
+    public function search($keyword){
+        $serach = $keyword['search'];
+        $search_cmd= array();
+        $search_cmd['command'] = 'search';
+        $search_cmd['query'] = $serach;
+        $search_cmd['bm'] = '15';
+        $search_cmd['sb'] = '1';
+        $search_cmd['no'] = '25';
+        $search_cmd['p'] = '0';
+        $search_cmd['col'] = 1;
+       //Creating the json object with all data (this is usually wrapped in a subroutine since its always the same)
+        $json_object = array();
+        $json_object["api_key"] = $apiKey;
+        $json_object["ver"] = 1;
+        $json_object["commands_json"] = json_encode( array($search_cmd) );
+
+        //NOTE: the commands_hash must always have the string 'dragspel' appended
+        $json_object["commands_hash"] = md5($json_object["commands_json"] . $apiSecret . 'dragspel');
+
+        $data_req = json_encode($json_object);
+
+        //the post argument
+        $post_val = "api=" . urlencode($data_req);
+        
+            $client = new Client(); //GuzzleHttp\Client
+            $response = $client->post($this->url, [
+                'headers'=>[
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'Accept-Version'=>'1.0'
+                ],
+                'form_params' => [$post_val]
+            ]);
+            if ($response->getBody()) {
+                $contents = json_decode($response->getBody(), true);
+                //$contents = $response->getBody();
+                return $contents;
+
+            }
+ }
+
+
+}
+
+?>
