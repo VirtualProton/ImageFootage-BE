d04da6e8c51be36cd423d18644aa4eb80b72c23d
amit <amitp@pixelvide.com>
2019-12-09 11:33:30 +0530

    changes

--- frontend/dist/main-es5.js
+++ frontend/dist/main-es5.js
@@ -1546,7 +1546,7 @@ var HeroService = /** @class */ (function () {
     function HeroService(http, messageService) {
         this.http = http;
         this.messageService = messageService;
-        this.heroesUrl = 'http://localhost/imagefootagenew/backend/api/'; // URL to web api
+        this.heroesUrl = 'http://ec2-18-218-154-217.us-east-2.compute.amazonaws.com/backend/api/'; // URL to web api
         this.localhostUrl = 'http://localhost/backend/api/';
         this.carouselImagesUrl = 'api/carouselImages';
         this.aosImagesUrl = 'api/aosImages';
