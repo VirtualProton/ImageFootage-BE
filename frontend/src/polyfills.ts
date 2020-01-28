22782d691174d7d984c8eba9557b0af1b545ab33
AKSrinivas <srinivas@conceptualpictures.com>
2020-01-28 16:36:11 +0530

    poster to background video

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -60,3 +60,4 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
