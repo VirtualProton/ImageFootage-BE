6796280f58a21a2d6added7df6b17bfd0b17538f
AKSrinivas <srinivas@conceptualpictures.com>
2020-04-13 18:12:59 +0530

    back button in back button

--- backend/resources/views/admin/package/apiquota.blade.php
+++ backend/resources/views/admin/package/apiquota.blade.php
@@ -20,7 +20,7 @@
         	<div class="col-md-12">
          		<div class="box box-primary">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Create API Quata</h3>
+                  <h3 class="box-title">Create API Quata</h3><a href="{{ URL::to('admin/api_quota_list') }}" class="btn pull-right">Back</a>
                 </div>
                @if( Session::has( 'success' ))
      			{{ Session::get( 'success' ) }}
