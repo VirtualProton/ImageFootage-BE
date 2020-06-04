8a4838b4110a1c1662c8f2ee6fb288f0e55960b2
vasi <vasi_eagle@hotmail.com>
2020-06-04 17:32:03 +0530

    post add product api

--- backend/app/Http/Controllers/Admin/ProductController.php
+++ backend/app/Http/Controllers/Admin/ProductController.php
@@ -984,10 +984,258 @@ ini_set('max_execution_time', '0'); // for infinite time of execution
 
    }
 
-   public function postAddProduct(){
-   		echo "hi postAddProduct"; die;
-   		
+   public function postAddProduct(Request $request){
+   		//echo "hi postAddProduct"; die;
+   		if($request->file('product_image')){
+   		$image = $request->file('product_image');
+		$name = time().'.'.$image->getClientOriginalExtension();
+   		 //print_r($name); die;
+   		}
+		
+		 $product = new Product;
+		 $productimages=new ProductImages;
+		 $file = $request->file('product_image');
+		 $data = getimagesize($file);
+ 		 $dimenctions = $data[0].'x'.$data[1];
+		 //Get the first three characters using substr.
+		 $firstThreeCharacters = substr($request->owner_name, 0, 3);
+		 $firstThreeCharactersType = substr($request->product_type, 0, 3);
+		 $productid=$firstThreeCharacters.$firstThreeCharactersType;
+		 $product->product_id =$productid;
+		 $product->product_title=$request->product_title;
+		 $product->license_type=$request->license_type;
+		 $product->product_category=$request->product_category;
+		 $product->product_subcategory=$request->product_sub_category;
+		 $product->product_owner=$request->owner_name;
+		 $product->product_title=$request->product_title;
+		 $product->product_vertical=$request->product_vertical;
+		 $product->product_keywords=$request->prodect_keywords;
+		 $product->product_thumbnail=$request->product_thumbnail;
+		 $product->product_size=$dimenctions;
+		 $product->product_release_details=$request->release_details;
+		 $product->product_price_small=$request->Price_small;
+		 $product->product_price_medium=$request->price_medium;
+		 $product->product_price_large=$request->price_large;
+		 $product->product_price_extralarge=$request->price_extra_large;
+		 $product->product_main_type=$request->product_type;
+		 $product->product_added_by=Auth::guard('admins')->user()->id;
+		 if(isset($request->sub_product_type) && !empty($request->sub_product_type)){
+		 	$product->product_sub_type=$request->sub_product_type;
+		 }
+		 $product->product_added_on=date('Y-m-d');
+		 $result=$product->save();
+		 $last_id=$product->id;
+		 if($result){
+			 /* for filters */
+			 $product_color=$request->product_color;
+			 if(isset($product_color) && !empty($product_color)){
+				 foreach($product_color as $key=>$pc){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_color';
+					   $productFilters->filter_type_id=$pc;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				 }
+			 }
+		 	 $product_gender=$request->product_gender;
+			 if(isset($product_gender) && !empty($product_gender)){
+				   foreach($product_gender as $key=>$pg){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_gender';
+					   $productFilters->filter_type_id=$pg;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				   }
+			 }
+		 	 $product_glow_type=$request->product_glow_type;
+			 if(isset($product_glow_type) && !empty($product_glow_type)){
+				  foreach($product_glow_type as $key=>$pgt){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_glow_type';
+					   $productFilters->filter_type_id=$pgt;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+		 	 $product_image_size=$request->product_image_size;
+			 if(isset( $product_image_size) && !empty( $product_image_size)){
+				  foreach($product_image_size as $key=>$pis){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_image_size';
+					   $productFilters->filter_type_id=$pis;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+		 	 $product_image_age=$request->product_image_age;
+			 if(isset($product_image_age) && !empty($product_image_age)){
+				  foreach($product_image_age as $key=>$pia){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_image_age';
+					   $productFilters->filter_type_id=$pia;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			  $product_locations=$request->product_locations;
+			 if(isset($product_locations) && !empty($product_locations)){
+				  foreach($product_locations as $key=>$loc){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_locations';
+					   $productFilters->filter_type_id=$loc;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			  $product_ethinicities=$request->product_ethinicities;
+			 if(isset($product_ethinicities) && !empty($product_ethinicities)){
+				  foreach($product_ethinicities as $key=>$eth){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_ethinicities';
+					   $productFilters->filter_type_id=$eth;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			 $product_peoples=$request->product_peoples;
+			 if(isset($product_peoples) && !empty($product_peoples)){
+				  foreach($product_peoples as $key=>$peop){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_peoples';
+					   $productFilters->filter_type_id=$peop;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			 
+			 
+			  $product_orientations=$request->product_orientations;
+			 if(isset($product_orientations) && !empty($product_orientations)){
+				  foreach($product_ethinicities as $key=>$eth){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_orientations';
+					   $productFilters->filter_type_id=$eth;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			  $product_resolution=$request->product_resolution;
+			 if(isset($product_resolution) && !empty($product_resolution)){
+				  foreach($product_resolution as $key=>$eth){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_resolution';
+					   $productFilters->filter_type_id=$eth;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			  $product_sort_types=$request->product_sort_types;
+			 if(isset($product_sort_types) && !empty($product_sort_types)){
+				  foreach($product_sort_types as $key=>$eth){
+					   $productFilters=new ProductFilters;
+					   $productFilters->filter_product_id=$last_id;
+					   $productFilters->filter_type='product_sort_types';
+					   $productFilters->filter_type_id=$eth;
+					   $productFilters->filter_added_by=Auth::guard('admins')->user()->id;
+					   $productFilters->filter_added_on=date('Y-m-d H:i:s');
+					   $result=$productFilters->save();
+				  }
+			 }
+			 
+			 /* end filters */
+			 
+			 
+			 if($last_id < 10){
+				 $last_id='00'.$last_id;
+			 }else if($last_id < 100){
+				 $last_id='0'.$last_id;
+			 }
+			 if($request->hasFile('product_image')) {
+				$image = $request->file('product_image');
+				$name = time().'.'.$image->getClientOriginalExtension();
+				$files2bucketemp= $image->getPathName();
+				$file_path='';
+				if($request->product_type=='Image'){
+					if($request->sub_product_type=='Vector'){
+						$file_path.='image/vector/';
+					}else if($request->sub_product_type=='Illustrator'){
+						$file_path.='image/illustrator/';
+					}else{
+						$file_path.='image/photo/';
+					}
 
+				}else if($request->product_type=='Footage'){
+					$file_path.='footage/';
+				}else if($request->product_type=='Editorial'){
+					if($request->sub_product_type=='Vector'){
+						$file_path.='editorial/vector/';
+					}else if($request->sub_product_type=='Illustrator'){
+						$file_path.='editorial/illustrator/';
+					}else{
+						$file_path.='editorial/photo/';
+					}
+				}
+				$destinationPath = public_path($file_path);
+				//$image->move($destinationPath, $name);
+				$s3Client = new S3Client([
+					/*'profile' => 'default',*/
+					'region' => 'us-east-2',
+					'version' => '2006-03-01'
+				]);
+				// Use multipart upload
+				//print_r($files2bucketemp);
+				//exit();
+				$finelname=$file_path.$name;
+				$source = $files2bucketemp;
+				$uploader = new MultipartUploader($s3Client, $source, [
+					'bucket' => 'imgfootage',
+					'key' => $finelname,
+				]);
+				
+				try {
+					$fileupresult = $uploader->upload();
+				} catch (MultipartUploadException $e) {
+					echo $e->getMessage() . "\n";
+				}
+    		 }
+			 $productid=strtolower($firstThreeCharacters.$firstThreeCharactersType.$last_id);
+			 $product_update = Product::find($last_id);
+			 $product_update->product_id=$productid;
+			 $product_update->product_main_image=$fileupresult['ObjectURL'];
+			 $product_update->save();
+			 
+			 $productimages->image_name=$name;
+			 $productimages->image_product_id=$last_id;
+			 $productimages->image_added_on=date('Y-m-d H:i:s');
+			 $productimages->image_added_by=Auth::guard('admins')->user()->id;
+			 $productimages->save();
+			 
+
+			 echo '{"status":"1","message":"Product Upload successful"}';
+		 }else{
+			echo '{"status":"0","message":"Some problem occured."}';
+		 }
+			
    }
    
 
