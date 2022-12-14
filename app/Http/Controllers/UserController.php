20da6df33d00f84eacecf453496820aa6551e9e6
ankitchandak <ankit.chandak@bytestechnolab.com>
2022-12-14 15:02:59 +0530

    env file set for reset password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -104,6 +104,7 @@ class UserController extends Controller
         return response()->json($result);
     }*/
     public function validUser(Request $request){
+        $hostname = env('FRONT_END_URL');
         $count = User::where('email','=',$request['email']['user_email'])->count();
 		if($count>0){
             $randnum=rand(1000,10000);
@@ -111,7 +112,7 @@ class UserController extends Controller
 			$update_array=array('otp'=>$randnum);
 		    $result = User::where('email',$request['email']['user_email'])->update($update_array);
 			// $url = 'https://imagefootage.com/resetpassword/'.$randnum.'/'.$request['email']['user_email'];
-            $url=url("resetpassword/".$randnum.'/'.$request['email']['user_email']);
+            $url = $hostname."/resetpassword/".$randnum."/".$request['email']['user_email'];
             $data = array('url'=>$url,'email'=>$request['email']['user_email']);
 				 Mail::send('email.forgotpassword', $data, function($message) use($data) {
                      $message->to($data['email'], '')->subject('Image Footage Forget Password')
