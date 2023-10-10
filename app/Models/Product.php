cdcf6bf9ed9568f3a3138260a16a8d31d09def5f
anjalibytes <anjali.modi@bytestechnolab.com>
2023-10-10 11:40:43 +0530

    Applie changes for music data

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -319,6 +319,10 @@ class Product extends Model
                 $query->where('product_main_type', '=', $type);
             });
 
+            if(isset($keyword['category_id']) && !empty($keyword['category_id'])){
+                $data->where('product_category',$keyword['category_id']);
+            }
+
             if (!empty($keyword['search'])) {
                 $data->where(function ($query) use ($search) {
                     $query->orWhere('product_id', '=', $search)
