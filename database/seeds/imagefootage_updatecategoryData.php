3d8f952881f0fb84d2f68c55df62366378a4a4aa
anjalibytes <anjali.modi@bytestechnolab.com>
2023-10-26 11:48:56 +0530

    Worked on comments for PR

--- database/seeds/imagefootage_updatecategoryData.php
+++ database/seeds/imagefootage_updatecategoryData.php
@@ -12,38 +12,38 @@ class imagefootage_updatecategoryData extends Seeder
      */
     public function run()
     {
-       $getAllData =  DB::table("imagefootage_productcategory")->get();
-       foreach ($getAllData as $key => $value) {
-        DB::table('imagefootage_productcategory')->insert([
-            'category_name' => $value->category_name,
-            'category_order'=> $value->category_order,
-            'category_added_by' => $value->category_added_by,
-            'is_display_home' => $value->is_display_home,
-            'category_status' => $value->category_status,
-            'category_added_on' => Carbon::now(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            'category_keywords' => $value->category_keywords,
-            'product_id' => $value->product_id,
-            'image_path' => $value->image_path,
-            'category_slug' => $value->category_slug,
-            'type'=>2
-        ]);
-        DB::table('imagefootage_productcategory')->insert([
-            'category_name' => $value->category_name,
-            'category_order'=> $value->category_order,
-            'category_added_by' => $value->category_added_by,
-            'is_display_home' => $value->is_display_home,
-            'category_status' => $value->category_status,
-            'category_added_on' => Carbon::now(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            'category_keywords' => $value->category_keywords,
-            'product_id' => $value->product_id,
-            'image_path' => $value->image_path,
-            'category_slug' => $value->category_slug,
-            'type'=>3
-        ]);
-       }
+        $getAllData =  DB::table("imagefootage_productcategory")->get();
+        foreach ($getAllData as $key => $value) {
+            DB::table('imagefootage_productcategory')->insert([
+                'category_name' => $value->category_name,
+                'category_order'=> $value->category_order,
+                'category_added_by' => $value->category_added_by,
+                'is_display_home' => $value->is_display_home,
+                'category_status' => $value->category_status,
+                'category_added_on' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'category_keywords' => $value->category_keywords,
+                'product_id' => $value->product_id,
+                'image_path' => $value->image_path,
+                'category_slug' => $value->category_slug,
+                'type'=>2
+            ]);
+            DB::table('imagefootage_productcategory')->insert([
+                'category_name' => $value->category_name,
+                'category_order'=> $value->category_order,
+                'category_added_by' => $value->category_added_by,
+                'is_display_home' => $value->is_display_home,
+                'category_status' => $value->category_status,
+                'category_added_on' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'category_keywords' => $value->category_keywords,
+                'product_id' => $value->product_id,
+                'image_path' => $value->image_path,
+                'category_slug' => $value->category_slug,
+                'type'=>3
+            ]);
+        }
     }
 }
