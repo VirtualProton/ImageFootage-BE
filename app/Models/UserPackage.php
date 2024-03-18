460ad60f43f428f74845082d87b4be7d49f6b60f
anjalibytes <anjali.modi@bytestechnolab.com>
2024-03-18 18:59:37 +0530

    Worked on feedback points

--- app/Models/UserPackage.php
+++ app/Models/UserPackage.php
@@ -9,7 +9,7 @@ class UserPackage extends Model
 {
     protected $table = 'imagefootage_user_package';
 
-    protected $fillable = ['transaction_id', 'user_id', 'package_id', 'package_name', 'package_price', 'package_description', 'package_products_count', 'package_type', 'package_expiry', 'package_plan', 'package_permonth_download', 'package_pcarry_forward', 'invoice', 'package_expiry_yearly', 'package_expiry_date_from_purchage', 'downloaded_product', 'pacage_size', 'payment_status', 'payment_mode', 'payment_gatway_provider', 'response_payment', 'status', 'order_type', 'footage_tier', 'created_at', 'updated_at', 'rozor_pay_id'];
+    protected $fillable = ['transaction_id', 'user_id', 'package_id', 'package_name', 'package_price', 'package_description', 'package_products_count', 'package_type', 'package_expiry', 'package_plan', 'package_permonth_download', 'package_pcarry_forward', 'invoice', 'package_expiry_yearly', 'package_expiry_date_from_purchage', 'downloaded_product', 'pacage_size', 'payment_status', 'payment_mode', 'payment_gatway_provider', 'response_payment', 'status', 'order_type', 'footage_tier', 'created_at', 'updated_at', 'rozor_pay_id','status'];
 
     public function downloads()
     {
