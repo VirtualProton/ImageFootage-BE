d917ab80792fa764cbb5f59fcd6a0d281d0105dc
Ravi Prajapati <ravi.prajapati@bytestechnolab.com>
2024-03-22 16:14:26 +0530

    Migration issue fixes

--- database/migrations/2024_03_21_063728_add_third_party_data_count_to_trending_words_table.php
+++ database/migrations/2024_03_21_063728_add_third_party_data_count_to_trending_words_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddThirdPartyDataCountToTrendingKeywordsTable extends Migration
+class AddThirdPartyDataCountToTrendingWordsTable extends Migration
 {
     /**
      * Run the migrations.
