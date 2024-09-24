4816e54055297cf23e6473b52a2cdb03d1a173f8
Taral Nagar <taral.nagar@bytestechnolab.com>
2024-09-24 14:26:26 +0530

    removed mongo config changes

--- config/database.php
+++ config/database.php
@@ -91,23 +91,14 @@ return [
             'prefix_indexes' => true,
         ],
 
-        // 'mongodb' => [
-        //     'driver'   => 'mongodb',
-        //     'host'     => env('MONGO_DB_HOST', 'localhost'),
-        //     'port'     => env('MONGO_DB_PORT', 27017),
-        //     'database' => env('MONGO_DB_DATABASE'),
-        //     'username' => env('MONGO_DB_USERNAME'),
-        //     'password' => env('MONGO_DB_PASSWORD'),
-        //     'options'  => []
-        // ],
-
         'mongodb' => [
             'driver'   => 'mongodb',
-            'dsn'      => env('MONGO_DB_URI'),  // Use the full MongoDB URI from the .env file
-            'database' => 'imagefootages_mongo', // Specify the database name directly
-            'options'  => [
-                'ssl' => true,  // If SSL is required for your connection
-            ],
+            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
+            'options'  => []
         ],
     ],
 
