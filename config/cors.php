6a5aad7a1b4d5a96f16a991a7e2db325558df08c
Pritham <impritham@gmail.com>
2025-12-21 22:59:27 +0530

    gst auth fix

--- config/cors.php
+++ config/cors.php
@@ -4,5 +4,6 @@ return [
     'allowed_origins' => [
         'staging'    => ['https://staging.imagefootage.com'],
         'production' => ['https://imagefootage.com'],
+        'local'      => ['https://imagefootage.com']
     ],
 ];
