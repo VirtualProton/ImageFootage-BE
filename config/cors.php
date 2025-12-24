5edb5283146a05a9368d9856ce1076e661a9317e
Pritham <impritham@gmail.com>
2025-12-25 00:28:34 +0530

    email service fixed

--- config/cors.php
+++ config/cors.php
@@ -4,5 +4,6 @@ return [
     'allowed_origins' => [
         'staging'    => ['https://staging.imagefootage.com'],
         'production' => ['https://imagefootage.com'],
+        'local' => ['http://localhost:3000', "https://imagefootage.com"],
     ],
 ];
