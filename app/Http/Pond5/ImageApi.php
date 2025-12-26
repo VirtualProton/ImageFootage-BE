c53ce00ea15b41e4bb2331245925a9dc8acfa2db
rajjalan <71684416+rajjalan@users.noreply.github.com>
2025-12-26 14:17:46 +0530

    Remove 'product_editorial' from filters

--- app/Http/Pond5/ImageApi.php
+++ app/Http/Pond5/ImageApi.php
@@ -58,7 +58,7 @@ class ImageApi
             $sort = 'default';
         }
         
-        $getFilters     = Arr::except($getKeyword, ['search', 'productType', 'pagenumber', 'product_editorial']);
+        $getFilters     = Arr::except($getKeyword, ['search', 'productType', 'pagenumber']);
         $filter_mapping = "";
 
         $url            = [];
