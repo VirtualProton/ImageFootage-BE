b64c749122f2612e64a1b81c00d767dad9e66875
Raj Jalan <rajjalan000@gmail.com>
2026-04-09 21:09:15 +0530

    Thus 2nd commit

---
+++ database/migrations/2026_04_09_000001_update_footage_resolution_filter_values.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class UpdateFootageResolutionFilterValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Get the resolution filter ID for footage type
+        $resolutionFilter = DB::table('imagefootage_filters')
+            ->where('value', 'resolution')
+            ->where('type', 'footage')
+            ->first();
+
+        if ($resolutionFilter) {
+            // Update hd to 1080
+            DB::table('imagefootage_filters_options')
+                ->where('filter_id', $resolutionFilter->id)
+                ->where('value', 'hd')
+                ->update(['value' => '1080']);
+
+            // Update 720p to 720
+            DB::table('imagefootage_filters_options')
+                ->where('filter_id', $resolutionFilter->id)
+                ->where('value', '720p')
+                ->update(['value' => '720']);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Get the resolution filter ID for footage type
+        $resolutionFilter = DB::table('imagefootage_filters')
+            ->where('value', 'resolution')
+            ->where('type', 'footage')
+            ->first();
+
+        if ($resolutionFilter) {
+            // Revert 1080 back to hd
+            DB::table('imagefootage_filters_options')
+                ->where('filter_id', $resolutionFilter->id)
+                ->where('value', '1080')
+                ->update(['value' => 'hd']);
+
+            // Revert 720 back to 720p
+            DB::table('imagefootage_filters_options')
+                ->where('filter_id', $resolutionFilter->id)
+                ->where('value', '720')
+                ->update(['value' => '720p']);
+        }
+    }
+}
