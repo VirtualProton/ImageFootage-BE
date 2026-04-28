f82d9407c1881efab9be3e37d334221554aed771
Raj Jalan <rajjalan000@gmail.com>
2026-04-28 12:06:30 +0530

    Tues 5th commit

--- resources/views/email/quotation.php
+++ resources/views/email/quotation.php
@@ -43,8 +43,7 @@
     $taxAmount = (float) ($quotation[0]['tax'] ?? 0);
     $totalAmount = (float) ($quotation[0]['total'] ?? 0);
     $subTotal = max($totalAmount - $taxAmount, 0);
-    // $discountAmount = (float) ($quotation[0]['discount_amount'] ?? 0);
-    $discountAmount = 0.00;
+    $discountAmount = (float) ($quotation[0]['discount_amount'] ?? 0);
     $currencySymbol = (($quotation[0]['currency'] ?? 'INR') === 'USD') ? '$' : '&#8377;';
     $totalItems = count($quotation);
     $amountInWordsText = trim((string) ($amount_in_words ?? ''));
